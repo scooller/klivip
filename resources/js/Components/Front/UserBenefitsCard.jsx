03c4e5984201d8b76ec15ca5b939fa3a20da7fd2
scooller <scoollerx@hotmail.com>
2026-06-21 23:24:15 -0400

    Update UserBenefitsCard.jsx

--- resources/js/Components/Front/UserBenefitsCard.jsx
+++ resources/js/Components/Front/UserBenefitsCard.jsx
@@ -1,5 +1,6 @@
 import CouponCard from './CouponCard';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTicket } from '@fortawesome/free-solid-svg-icons';
 
 export default function UserBenefitsCard({
     activeCoupons = [],
