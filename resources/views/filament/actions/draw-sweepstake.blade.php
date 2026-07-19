8f86fb167226d43b93ca90f53215a7288922eafb
scooller <scoollerx@hotmail.com>
2026-07-19 19:25:45 -0400

    Update draw-sweepstake.blade.php

--- resources/views/filament/actions/draw-sweepstake.blade.php
+++ resources/views/filament/actions/draw-sweepstake.blade.php
@@ -61,6 +61,7 @@
                 class="block mx-auto rounded-full shadow-lg bg-white dark:bg-gray-900"
                 style="width: 340px; height: 340px;"
             ></canvas>
+            <input type="text" name="winner-preview" readonly class="mt-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm" />
         </div>
 
         <div x-show="winners.length > 0" x-transition class="mt-2">
